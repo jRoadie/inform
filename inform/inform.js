5f627685e8cecfead3963cd0e6715c6eb32fd4b4
Akter Hossain Sohag <akterhossain.dsi@gmail.com>
2014-09-16 19:05:27 +0600

    simple modification

--- inform/inform.js
+++ inform/inform.js
@@ -1,9 +1,26 @@
 (function($) {
     var version = 1.0;
+    var Field = function() {};
+    Field.newInstance = function(args) {
+        return $.extend(true, {}, new Field(), $.fn.inform.fieldPrototype, args);
+    };
+    var Text = 456;
     $.fn.inform = function(options) {
-        var conf = $.extend({}, $.fn.inform.options, options);
+        var conf = $.extend(true, $.fn.inform.options, options);
         return this;
     };
     $.fn.inform.options = {};
+    $.fn.inform.defaultFieldAttrs = {
+        name: undefined,
+        value: undefined,
+        id: undefined,
+        'class': undefined
+    };
+    $.fn.inform.fieldPrototype = {
+        tag: undefined,
+        uid: undefined,
+        attrs: $.fn.inform.defaultFieldAttrs,
+        data: {}
+    };
     $('.inform, #inform').inform();
 })(jQuery);
